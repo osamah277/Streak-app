270247736e2423a3825f3acf2d06be3a09f375c9
Osamah Hasan <77911801+osamah277@users.noreply.github.com>
2026-04-01 18:02:18 +0000

    small changes

--- verses.js
+++ verses.js
@@ -1,4 +1,5 @@
 const VERSES = [
+   // al Fatihah
   {arabic:'بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ', translation: 'In the Name of Allah—the Most Compassionate, Most Merciful.', ref:'Surah Al-Fatihah 1:1'},
   {arabic:'ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَـٰلَمِينَ', translation: 'All praise is for Allah—Lord of all worlds,', ref:'Surah Al-Fatihah 1:2'},
   {arabic:'ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ', translation:'the Most Compassionate, Most Merciful,', ref:'Surah Al-Fatihah 1:3'},
@@ -6,4 +7,6 @@ const VERSES = [
   {arabic:'إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ', translation:'You ˹alone˺ we worship and You ˹alone˺ we ask for help.', ref:'Surah Al-Fatihah 1:5'},
   {arabic:'ٱهْدِنَا ٱلصِّرَٰطَ ٱلْمُسْتَقِيمَ', translation:'Guide us along the Straight Path,', ref:'Surah Al-Fatihah 1:6'},
   {arabic:'صِرَٰطَ ٱلَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ ٱلْمَغْضُوبِ عَلَيْهِمْ وَلَا ٱلضَّآلِّينَ', translation:'the Path of those You have blessed—not those You are displeased with, or those who are astray', ref:'Surah Al-Fatihah 1:7'},
+
+  // al Baqarah
 ];
