a15719f49b8921f995b32ab74bcea74111598061
Osamah Hasan <77911801+osamah277@users.noreply.github.com>
2026-04-01 17:29:40 +0000

    vers update

--- verses.js
+++ verses.js
@@ -1,3 +1,3 @@
 const VERSES = [
-  
+  {arabic:'بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ', translation: 'In the Name of Allah—the Most Compassionate, Most Merciful.', ref:'Surah Al-Fatihah 1:1'},
 ];
